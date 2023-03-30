9293ceac47a8624d710d96c7cb6666c1397ab75c
Yui Li <dyuili@163.com>
2023-03-30 11:47:16 +0800

    YoeeWeb version1.0

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="root">
+    <h1>APP组件</h1>
+    <Suspense>
+      <template v-slot:default>
+        <Child></Child>
+      </template>
+      <template v-slot:fallback>
+        <h2>Loading...</h2>
+      </template>
+    </Suspense>
+  </div>
+</template>
+
+<script>
+import { defineAsyncComponent } from 'vue'
+const Child = defineAsyncComponent(() =>import('./components/Child.vue'))
+import { reactive, provide, toRefs } from 'vue'
+// import Child from './components/Child.vue'
+
+export default {
+  name: 'App',
+  components: {Child},
+
+  setup(){
+    const person = reactive({
+      name: 'Yui',
+      age: 24,
+    })
+    
+    return { ...toRefs(person)}
+  }
+}
+</script>
+
+<style>
+  #root {
+    height: 500px;
+    background-color: skyblue;
+  }
+</style>
---
+++ src/components/Child.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="child">This is child component</div>
+</template>
+
+<script>
+export default {
+  name: 'Child'
+}
+</script>
+
+<style>
+  #child {
+    height: 300px;
+    background-color: pink;
+  }
+</style>
